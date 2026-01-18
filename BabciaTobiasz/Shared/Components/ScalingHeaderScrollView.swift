eaa0b0b09d48a09dc32d70f2688cb1dcd2743daf
dietpi <dietpi@Mac.lan>
2026-01-18 01:05:14 +0000

    fix: Swift 6 concurrency error in ScalingHeaderScrollView
    Added nonisolated(unsafe) to PreferenceKey defaultValue

--- BabciaTobiasz/Shared/Components/ScalingHeaderScrollView.swift
+++ BabciaTobiasz/Shared/Components/ScalingHeaderScrollView.swift
@@ -6,7 +6,7 @@ import SwiftUI
 private let scalingHeaderScrollSpace = "ScalingHeaderScrollView"
 
 private struct ScalingHeaderScrollOffsetKey: PreferenceKey {
-    static var defaultValue: CGFloat = 0
+    nonisolated(unsafe) static var defaultValue: CGFloat = 0
     static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
         value = nextValue()
     }
